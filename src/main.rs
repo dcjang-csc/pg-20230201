c92f155d2d91d40f88702677a691a5d812a139c0
dcjang-csc <dcjang.csc@gmail.com>
2023-02-03 12:47:32 +0100

    20230203

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Guess the number!");
-    println!("Please input your guess.");
+    
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
+        let mut guess = String::new();
+
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse().expect("please type a number!");
-    println!("guess: {guess}");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("guess: {guess}");
 
-    match guess.cmp(&secret_number) {
-        cmp::Ordering::Less => println!("Too small"),
-        cmp::Ordering::Greater => println!("Too big"),
-        cmp::Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            cmp::Ordering::Less => println!("Too small"),
+            cmp::Ordering::Greater => println!("Too big"),
+            cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
     println!("secre number: {secret_number}");
-
-    
 }
